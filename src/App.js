3fa90981c81b39369c9f903bb9a3755faf395c97
Lincoln Li <vivalkm@gmail.com>
2023-08-10 23:44:56 -0700

    implement database to CRUD books via json-server

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import CreateBook from "./components/CreateBook";
 import ReadingList from "./components/ReadingList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
     const [books, setBooks] = useState([]);
-    const handleCreateBook = (bookName) => {
-        const newBook = { title: bookName, id: Math.round(Math.random() * 100000) };
-        setBooks([...books, newBook]);
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+        setBooks(response.data);
     };
 
-    const deleteBookByID = (id) => {
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const handleCreateBook = async (bookName) => {
+        const newBook = await axios.post("http://localhost:3001/books", { title: bookName });
+        setBooks([...books, newBook.data]);
+    };
+
+    const deleteBookByID = async (id) => {
+        const deletedBook = await axios.delete(`http://localhost:3001/books/${id}`);
+        console.log(deletedBook);
         const newBooks = books.filter((book) => {
-            return book.id !== id;
+            return id !== book.id;
         });
         setBooks(newBooks);
     };
 
-    const editBookByID = (newTitle, id) => {
+    const editBookByID = async (newTitle, id) => {
+        const editedBook = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
         const newBooks = books.map((book) => {
             if (book.id !== id) {
                 return book;
             } else {
-                return { title: newTitle, id: id };
+                // to avoid overwriting other properties of the book that could be updated by other users concurrently
+                return {...book, ...editedBook.data};
             }
         });
         setBooks(newBooks);
--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -3,9 +3,7 @@ import BookShow from "./BookShow";
 
 export default function ReadingList({ books, onDelete, onEdit }) {
     const renderedBooks = books.map((book) => {
-        return (
-            <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
-        );
+        return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />;
     });
-    return <div class="book-list">{renderedBooks}</div>;
+    return <div className="book-list">{renderedBooks}</div>;
 }
