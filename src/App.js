bcf67eaae82d20e46cbe6f381a0fb9a82d4eb5a7
Lincoln Li <vivalkm@gmail.com>
2023-08-11 00:16:41 -0700

    add comments and env.js for db url

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import CreateBook from "./components/CreateBook";
 import ReadingList from "./components/ReadingList";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { dbUrl } from "./env.js";
 
 function App() {
     const [books, setBooks] = useState([]);
     const fetchBooks = async () => {
-        const response = await axios.get("http://localhost:3001/books");
+        const response = await axios.get(`${dbUrl}/books`);
         setBooks(response.data);
     };
 
+    // fetch books from db on the first render only
     useEffect(() => {
         fetchBooks();
     }, []);
 
     const handleCreateBook = async (bookName, bookImgUrl) => {
-        const newBook = await axios.post("http://localhost:3001/books", {
+        const newBook = await axios.post(`${dbUrl}/books`, {
             title: bookName,
             imgUrl: bookImgUrl,
         });
@@ -23,8 +25,7 @@ function App() {
     };
 
     const deleteBookByID = async (id) => {
-        const deletedBook = await axios.delete(`http://localhost:3001/books/${id}`);
-        console.log(deletedBook);
+        const deletedBook = await axios.delete(`${dbUrl}/books/${id}`);
         const newBooks = books.filter((book) => {
             return id !== book.id;
         });
@@ -32,7 +33,7 @@ function App() {
     };
 
     const editBookByID = async (newTitle, newImgUrl, id) => {
-        const editedBook = await axios.put(`http://localhost:3001/books/${id}`, {
+        const editedBook = await axios.put(`${dbUrl}/books/${id}`, {
             title: newTitle,
             imgUrl: newImgUrl,
         });
